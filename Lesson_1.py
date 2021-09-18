64f225789c204a458639ec17ef6a00b1321585cc
ekaterinatkachenko <Tkachenko.city@gmail.com>
2021-09-18 17:46:26 +0300

    add Lesson_1_all.py

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,3 +1,53 @@
-print("Hello, World")
-# comment
-print("Hello, World")
+# # приведение типов
+# value = 1
+# new_value = float(value)
+# print(new_value, type(new_value))
+
+
+# # bool_value = 2 > ("qwe" in "qwerty")
+#
+# some_value = 4
+# bool_value_1 = (2 != some_value)
+# bool_value = not bool_value_1
+#
+#
+# # >    <    >=    <=    ==(сравнение)      != (собака не кошка, НЕ РАВНО)
+# # ОПЕРАТОРЫ   not     and     or
+#
+#
+# print(bool_value, type(bool_value))
+
+
+
+
+# value_1 = '25'
+# value_2 = 0.5
+# #value = value_1 / value_2 #обычное деление
+# #value = value_1 // value_2 #целочисленное деление
+# # value = value_1 % value_2 #остаток от деления
+# # value = value_1 ** value_2 #возведение в степень. Корень для ** 0.5
+#
+# value = value_1 * value_2
+# print (value)  #, type(value))
+
+
+# value = '10'
+# value = 100
+# first = 'qwe'
+# second = "asd"
+# value = first
+# first = second
+# second = value
+# first, second = second, first
+# print (first, second)
+
+
+
+# print (value, type(value))
+
+
+
+# print("Hello, World")
+# # comment
+# print("Hello, World")
+# print(3*2-1, 75463, 915, "tut")
